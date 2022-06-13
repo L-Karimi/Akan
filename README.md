99c0b9d7033fb149ce496cf80a720d81d81e9280
L-Karimi <luciahkei101@gmail.com>
2022-06-13 19:33:23 +0300

    modified files

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # Author
+
 Lucy Wangeci Karimi
+
 # The Akan Naming System.
+
 #### Description.
+
 The Akan people from Ghana has a unique way of naming their children,which is according to the day of the week they were born which is independent on the gender of the child. This naming tradition is shared throughout West Africa and the African diaspora.This projects helps an individual to establish their name according to this Akan naming system.
+
 ## Setup/Installation Requirements
-* To run my page on your browser,
-* Git clone https://github.com/L-Karimi/Akan to your terminal
-* Open Documents and open the project with Vs code
-* Render the page to your browser by live server
+
+- To run my page on your browser,
+- Git clone https://github.com/L-Karimi/Akan to your terminal
+- Open the location of your project and open with Vs code
+- Render the page to your browser by live server
+
 ## Known Bugs
+
 There are no Known bugs
+
 ## Technologies Used
-* HTML
-* CSS
-* javasript
+
+- HTML
+- CSS
+- javasript
+
 ## Support and contact details
-* karimiluccy@gmail.com
-* 0797916750
+
+- karimiluccy@gmail.com
+- 0797916750
+
 ### License
- licensed under [MIT license](LICENSE)
+
+licensed under [MIT license](LICENSE)
--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,58 @@
-var dayOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var maleAkanName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var dayOfTheWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+var maleAkanName = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+var femaleAkanName = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama",
+];
 
 function akan() {
-    var year = document.getElementById("year").value;
-    var month = parseInt(document.getElementById("month").value);
-    var Date = parseInt(document.getElementById("date").value);
-    var gender = document.getElementById("gender").value;
-    var YY = parseInt(year.substr(2, 4)); 
-    var MM = month;
-    var DD = Date;
-    var day = parseInt((5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7
+  var year = document.getElementById("year").value;
+  var month = parseInt(document.getElementById("month").value);
+  var Date = parseInt(document.getElementById("date").value);
+  var gender = document.getElementById("gender").value;
+  var YY = parseInt(year.substr(2, 4));
+  var MM = month;
+  var DD = Date;
+  var day = parseInt((5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
 
-    if (gender === "Female") {
-        alert("Akan name is " + femaleAkanName[day])
-    } else {
-        alert("Akan name is " + maleAkanName[day])
-        
-    }
-    if (DD=>0||DD<=31) {
-        // alert("Valid date")
-    } else {
-        alert("Enter a valid date")
-    }
-    if (MM=>o||MM<=12) {
-        // alert("valid month")
-    } else {
-         alert("Enter a valid month")
-            }
-            if (YY=>1800||YY<=2020) {
-                // alert("valid year")
-            } else {
-                alert("Enter a valid year")
-            }
-           
-
-
+  if (gender === "Female") {
+    alert("Akan name is " + femaleAkanName[day]);
+  } else {
+    alert("Akan name is " + maleAkanName[day]);
+  }
+  if ((DD) => 0 || DD <= 31) {
+    // alert("Valid date")
+  } else {
+    alert("Enter a valid date");
+  }
+  if ((MM) => o || MM <= 12) {
+    // alert("valid month")
+  } else {
+    alert("Enter a valid month");
+  }
+  if ((YY) => 1800 || YY <= 2020) {
+    // alert("valid year")
+  } else {
+    alert("Enter a valid year");
+  }
 }
